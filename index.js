c1db17819419f194c216fcd045d5a8f31e763c1c
Shoaib <shoaibaslam3815@gmail.com>
2025-10-09 16:35:36 +0500

    first branch

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const port = process.env.port || 3000;
+const app = express();  
+ 
+app.use(express.static('./public'));
+
+const path_second = path.join(__dirname, "./source/views");
+const partial__path = path.join(__dirname, "./source/partials" ); 
+app.set("view engine", "hbs");    
+app.set("views", path_second);     
+hbs.registerPartials(partial__path);     
+
+        
+app.get("/",(req,res)=>{        
+  res.render("skeleton");  
+  console.log("skeleton api is running successfully")    
+});  
+ 
+app.get("/index",(req,res)=>{                   
+  res.render("index");  
+  console.log("index api is running successfully")    
+});
+ 
+app.get("/product",(req,res)=>{  
+  res.render("product"); 
+  console.log("product api is running successfully") 
+});         
+
+app.get("/contact",(req,res)=>{  
+  res.render("contact");  
+  console.log("contact page is running successfully") 
+});         
+// app.listen(port, () => { 
+//     console.log(`Server running at http://localhost:${port}`);
+//   });   
+ 
+ module.exports = index; 
