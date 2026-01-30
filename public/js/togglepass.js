7c90c7af5a006a1c09f9a3b18f2a34a822e601bf
Shoaib <shoaibaslam3815@gmail.com>
2026-01-30 17:44:51 +0500

    hope db is connect

--- public/js/togglepass.js
+++ public/js/togglepass.js
@@ -1,25 +1,48 @@
 function togglePassword(id, icon) {
-    const input = document.getElementById(id);
-  
-    if (input.type === "password") {
-      input.type = "text";
-      icon.classList.remove("bx-show");
-      icon.classList.add("bx-hide");
-    } else {
-      input.type = "password";
-      icon.classList.remove("bx-hide");
-      icon.classList.add("bx-show");
-    }
-  }
-  const notify = document.getElementById("notify");
+  const input = document.getElementById(id);
 
-  if (notify) {
-     setTimeout(() => {
-      notify.classList.add("show");
-    }, 100);
- 
-    setTimeout(() => {
-      notify.classList.remove("show");
-    }, 5000);
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.remove("bx-show");
+    icon.classList.add("bx-hide");
+  } else {
+    input.type = "password";
+    icon.classList.remove("bx-hide");
+    icon.classList.add("bx-show");
   }
-  
+}
+const notify = document.getElementById("notify");
+
+if (notify) {
+  setTimeout(() => {
+    notify.classList.add("show");
+  }, 100);
+
+  setTimeout(() => {
+    notify.classList.remove("show");
+  }, 5000);
+}
+
+document.querySelectorAll("input").forEach(input => {
+  const error = input.nextElementSibling;
+  const max = input.maxLength;
+  input.addEventListener("input", () => {
+    clearTimeout(input._timer);
+
+    if (input.value.length === max) {
+      error.textContent = `maximum use ${max} characters`;
+      error.style.background =  "#ffffff";
+      error.style.opacity = "1";
+
+      setTimeout(() => error.style.opacity = "0", 2800);
+
+      input._timer = setTimeout(() => {
+        error.textContent = "";
+      }, 3000);
+    } else {
+      error.textContent = "";
+    }
+  });
+
+});
+
